fc0b431139c8be96292adee15261522bd74ec34c
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-12-28 12:45:14 -0400

    Add componente Rodape

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <Banner />
   <ConteudoPrincipal />
+  <Rodape />
 </template>
 
 <script lang="ts">
   import Banner from './components/Banner.vue';
   import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+import Rodape from './components/Rodape.vue';
 
   export default {
-    components: { 
-      Banner, 
-      ConteudoPrincipal,
-    }
+    components: {
+    Banner,
+    ConteudoPrincipal,
+    Rodape
+}
   }
 </script>
 
---
+++ src/components/Rodape.vue
@@ -0,0 +1,52 @@
+<template>
+  <footer class="rodape paragrafo">
+    <span>&copy; Copyright 2023 - Desenvolvido por </span>
+    <a href="https://www.wandersontimoteo.com.br/" target="_blank"> Wanderson Timóteo </a>
+  </footer>
+</template>
+
+<style scoped>
+.rodape {
+  background: var(--verde-escuro, #263A29);
+  padding: 1.5rem 7.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.rodape a {
+  color: var(--creme);
+  margin-left: .35rem;
+}
+
+.rodape a:hover {
+  color: var(--coral);
+}
+
+@media only screen and (max-width: 1300px) {
+  .rodape {
+    padding: 1.5rem 3.75rem;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .rodape {
+    font-size: 1.1rem;
+    padding: 1.5rem;
+    display: block;
+    align-items: center;
+  }
+
+  .rodape span{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .rodape a{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
